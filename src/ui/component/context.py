27490e18de16081d699fde6afb745cb987d9b1d8
André "Intentor" Martins <andre@intentor.com.br>
2024-07-01 16:27:01 -0300

    Add saving of uploaded files

--- src/ui/component/context.py
+++ src/ui/component/context.py
@@ -1,11 +1,19 @@
-"""Context management components module."""
+"""Context management components module.
 
-import time
+Sessions:
+    - id: Session ID
+    - files: List of the uploaded files.
+"""
+
+import os
 import traceback
 from logging import getLogger
+from typing import List
 
 import streamlit as st
+from streamlit.runtime.uploaded_file_manager import UploadedFile
 
+from config import SESSION_PATH
 from llm.operator import LlmOperator
 from ui.component.base import OperationModeManager, UiComponent
 import ui.component.icon as icon
@@ -51,7 +59,9 @@ class ContextCompoonent(UiComponent):
                 if uploaded_files:
                     try:
                         with st.spinner('Indexing files...'):
-                            self._operator.index_files([])
+                            files_path = self._save_files(uploaded_files)
+                            st.session_state.files = files_path
+                            self._operator.index_files(files_path)
                             st.success('Files indexed', icon=icon.SUCCESS)
                             st.rerun()
                     except Exception as e:
@@ -60,12 +70,38 @@ class ContextCompoonent(UiComponent):
                         st.error(f"Error: {e}")
                         st.stop()
 
+    def _save_files(
+            self,
+            files: list[UploadedFile]
+    ) -> List[str]:
+        """Save a list of files in the disk.
+
+        Args:
+            files_path: Path where the files will be saved.
+            files: Files to save.
+
+        Returns:
+            Path of the uploaded files.
+        """
+        files_info: List[str] = []
+        session_dir = os.path.join(SESSION_PATH, st.session_state.id, 'files')
+        os.makedirs(session_dir, exist_ok=True)
+        for file_to_save in files:
+            file_path = os.path.join(session_dir, file_to_save.name)
+            logger.info('m=saving file=%s', file_path)
+
+            with open(file_path, 'wb') as file_new:
+                file_new.write(file_to_save.getvalue())
+                files_info.append(file_path)
+
+        return files_info
+
     def _render_list_context(self):
         """Render the listing of components for the context module."""
         with st.container(border=True):
-            st.write(f"Indexed files ( {len(st.session_state.files)})")
+            st.write(f"Indexed files ({len(st.session_state.files)})")
             for file in st.session_state.files:
-                st.caption(file.name)
+                st.caption(os.path.basename(file))
 
     def _has_files(self) -> bool:
         """Indicates whether the session has files."""
