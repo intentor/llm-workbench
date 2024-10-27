1c080754f90f77e391b786bd618d3a55c1bcb72f
André "Intentor" Martins <andre@intentor.com.br>
2024-10-27 18:47:36 -0300

    Update default generator to Ollama

--- src/config.py
+++ src/config.py
@@ -17,15 +17,13 @@ OPEN_ROUTER_HOST: str = os.getenv(
     'OPEN_ROUTER_HOST', 'https://openrouter.ai/api/v1/chat/completions')
 """Host of the OpenRouter API."""
 
-OPEN_ROUTER_KEY: str = os.getenv(
-    'OPEN_ROUTER_KEY', '<Replace with your key>')
-"""Key of the OpenRouter API."""
+OPEN_ROUTER_KEY: str = os.getenv('OPEN_ROUTER_KEY', '<Replace with your key>')
 
 OPEN_ROUTER_REQUEST_TIMEOUT: int = int(
     os.getenv('OPEN_ROUTER_TIMEOUT', '60000'))
 """Request timeout to the OpenRouter server, in miliseconds."""
 
-MODEL_GENERATOR: str = os.getenv('MODEL_GENERATOR', 'OPENROUTER')
+MODEL_GENERATOR: str = os.getenv('MODEL_GENERATOR', 'OLLAMA')
 """Generator used for LLM generation (OLLAMA, OPENROUTER). Defaults to OLLAMA."""
 
 MODEL_EMBEDDINGS: str = os.getenv('MODEL_EMBEDDINGS', 'nomic-embed-text')
